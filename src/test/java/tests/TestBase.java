fe1f1e41e4743757af3f1333217612deab50a26f
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-12 20:35:59 +0300

    add selenoid support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
@@ -11,5 +12,15 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        // для запуска на удаленном селеноиде
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        // конфигурация удаленного запуска
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        // чтобы было видно, что происходит
+        capabilities.setCapability("enableVNC", true);
+        // для записи видео
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
     }
 }
