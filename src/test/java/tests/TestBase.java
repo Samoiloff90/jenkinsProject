bb169c13d54314a994be00ed133fcd9d2c7ca46b
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-14 22:32:29 +0300

    test properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,30 +14,43 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.fileDownload = FOLDER;
-
-        //password and user for remote browser
         String user = System.getProperty("user");
         String password = System.getProperty("password");
+        String browserSize = System.getProperty("browserSize");
 
-        // конфигурация удаленного запуска
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = browserSize;  //"1920x1080"
+        Configuration.remote = "https://" + user + ":" + password + "@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        // чтобы было видно, что происходит
         capabilities.setCapability("enableVNC", true);
-        // для записи видео
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
 
-        // для запуска на удаленном селеноиде
-        Configuration.remote = "https://" + user + ":" + password + "@selenoid.autotests.cloud/wd/hub";
+//        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//        Configuration.fileDownload = FOLDER;
+//
+//        //password and user for remote browser
+//        String user = System.getProperty("user");
+//        String password = System.getProperty("password");
+//
+//        // конфигурация удаленного запуска
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        // чтобы было видно, что происходит
+//        capabilities.setCapability("enableVNC", true);
+//        // для записи видео
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.browserCapabilities = capabilities;
+//
+//        // для запуска на удаленном селеноиде
+//        Configuration.remote = "https://" + user + ":" + password + "@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
     void addAttachment() {
-        Attach.screenshotAs("Last screenshot");
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
